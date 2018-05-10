6d80fed93100161d08e14cb85f356a5b9366b810
Eric Baltutis <baltu008@umn.edu>
2018-05-10 02:06:10 -0500

    added href links using js

---
+++ js/projects.js
@@ -0,0 +1,27 @@
+$(document).ready(function () {
+
+  $('#hangmanBtn').on("click", function () {
+      window.open("https://baltu008.github.io/Hangman/");
+  });
+
+  $('#crystalBtn').on("click", function () {
+      window.open("https://baltu008.github.io/week-4-game/");
+  });
+
+  $('#starWarsBtn').on("click", function () {
+      window.open("https://baltu008.github.io/TriviaGame/");
+  });
+
+  $('#gifTasticBtn').on("click", function () {
+      window.open("https://baltu008.github.io/GifTastic/");
+  });
+
+  $('#trainScheduler').on("click", function () {
+      window.open("https://baltu008.github.io/TrainScheduler/");
+  });
+  
+  $('#nutritionProject').on("click", function () {
+      window.open("https://baltu008.github.io/nutritionProject/");
+  });
+
+});
