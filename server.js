483a6b86d5ca25eb0d152ba69df9eac647ec9836
Eric Baltutis <34050626+baltu008@users.noreply.github.com>
2018-07-17 17:01:55 -0500

    server changes add ejs

--- server.js
+++ server.js
@@ -1,30 +1,23 @@
 // Dependencies
-var http = require("http");
-var fs = require("fs");
+var express= require('express');
+var bodyParser = require('body-parser');
+var path = require('path');
 
-var PORT = 8080;
 
-// Create server
-var server = http.createServer(handleRequest);
+var app = express();
 
-function handleRequest(req, res) {
-  switch (request.url) {
-    case "/css/style.css":
-      res.writeHead(200, { "Content-Type": "text/css" });
-      res.write(cssFile);
-      break;
-    default:
-      response.writeHead(200, { "Content-Type": "text/html" });
-      response.write(htmlFile);
-  }
-  // Here we use the fs package to read our index.html file
-  // fs.readFile(__dirname + "/index.html", function(err, data) {
-  //   res.writeHead(200, { "Content-Type": "text/html" });
-    res.end(data);
-  // });
-}
+// Setup app to use packages
+app.use(bodyParser.json());
+app.set("view engine", "ejs");
+
+// index page 
+app.get('/', function(req, res) {
+  res.render('index');
+});
+
+var PORT = process.env.PORT || 8080;
 
 // Start server
-server.listen(PORT, function() {
+app.listen(PORT, function() {
   console.log("Server is listening on PORT: " + PORT);
 });
