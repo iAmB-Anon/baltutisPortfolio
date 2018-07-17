43397efd11e49300c20aa23befca8e557ee19bd3
Eric Baltutis <baltu008@umn.edu>
2018-07-17 18:01:16 -0500

    finish ejs

--- server.js
+++ server.js
@@ -4,15 +4,23 @@ var bodyParser = require('body-parser');
 var path = require('path');
 
 
+
 var app = express();
 
 // Setup app to use packages
 app.use(bodyParser.json());
 app.set("view engine", "ejs");
+app.use(express.static('public'))
 
 // index page 
 app.get('/', function(req, res) {
   res.render('index');
+});
+app.get('/projects', function(req, res) {
+  res.render('projects');
+});
+app.get('/contact', function(req, res) {
+  res.render('contact');
 });
 
 var PORT = process.env.PORT || 8080;
